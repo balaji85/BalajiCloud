b9344d9f7fdbfcb19629111eefad44358a025749
Balaji Nagayya <balaji.nagayya@gmail.com>
2024-07-02 20:17:14 +0530

    modified helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {//The main method begins here.
 public static void main(String []args) 
-  {System.out.println("Hello World 123"); // prints Hello World
+  {
+	System.out.println("Hello World 123");
+	System.out.println("Devops Class");
   }//main method ends here
   }//Hello world class ends here.
